00bd6e242dbca5ed655a4b5fd3b8ce4f935fe411
Fat-Llama <connorglennon@outlook.com>
2017-12-09 22:22:03 +0000

    Part 1

--- src/com/roundarch/codetest/part1/Part1Fragment.java
+++ src/com/roundarch/codetest/part1/Part1Fragment.java
@@ -1,9 +1,11 @@
 package com.roundarch.codetest.part1;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +14,14 @@ import android.widget.Switch;
 import android.widget.TextView;
 import com.roundarch.codetest.R;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by mdigiovanni on 8/15/13.
@@ -25,22 +31,72 @@ public class Part1Fragment extends Fragment {
     @BindView(R.id.sbTop)
     SeekBar mSeekBarTop;
     @BindView(R.id.sbBottom)
-    SeekBar getmSeekBarBottom;
+    SeekBar mSeekBarBottom;
     @BindView(R.id.switchSyncSeekBars)
     Switch mSwitchSyncSeekBars;
+    @BindView(R.id.tvDifference)
+    TextView mtvDifference;
 
-    //FIXME: Improve something! Anything
+    Unbinder mButterKnifeUnbinder;
+
+    int mTopValue, mBottomValue, mDifference;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_part1, null);
-        return view;
+        return inflater.inflate(R.layout.fragment_part1, null);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        // TODO - obtain references to your views from the layout
+        mButterKnifeUnbinder = ButterKnife.bind(this, view);
 
-        // TODO - hook up any event listeners that make sense for the task
+        mTopValue = mSeekBarTop.getProgress();
+        mBottomValue = mSeekBarBottom.getProgress();
+
+        addListener(mSeekBarTop, mSeekBarBottom);
+        addListener(mSeekBarBottom, mSeekBarTop);
+    }
+
+    private void addListener(SeekBar target, final SeekBar syncBuddy){
+        target.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            boolean isTracking = false;
+            int dI;
+            int lastValue;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if(isTracking) {
+                    int dI = i - lastValue;
+                    if (mSwitchSyncSeekBars.isChecked()) {
+                        syncBuddy.setProgress(syncBuddy.getProgress() + dI);
+                    }
+                    updateDifference();
+                }
+                lastValue = i;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                isTracking = true;
+                dI = 0;
+                lastValue = seekBar.getProgress();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                isTracking = false;
+            }
+        });
+    }
+
+    private void updateDifference(){
+        int difference = mSeekBarTop.getProgress() - mSeekBarBottom.getProgress();
+        mtvDifference.setText("" + difference);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mButterKnifeUnbinder.unbind();
     }
 }
